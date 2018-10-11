9ccd427c77cdc87ed30254dfbb8fbef427af2209
Putri1987 <taniamischa87@gmail.com>
2018-10-11 23:52:13 +0700

    dataobjekq

---
+++ dataobjek.php
@@ -0,0 +1,15 @@
+
+<?php
+class Sapa{
+	var $variable;
+
+}
+
+// membuat objek pesan . ini disebut instansiasi
+$pesan = new Sapa ();
+
+
+$pesan->variable="Hello Mabes TNI"; //memberikan nilai untuk properti str pada objek pesan
+echo $pesan->variable; // mencetak nilainya (pengolahan data)
+
+?>
