e00a412ad39056873b4d2fda36280bf03de58f98
Putri1987 <taniamischa87@gmail.com>
2018-10-12 00:14:11 +0700

    dataobjek3

---
+++ dataobjek3.php
@@ -0,0 +1,29 @@
+<?php
+
+$pesan = new Sapa();
+$pesan->variable = "Hello Mabes TNI ";
+echo $pesan->variable;
+$pesan->variable = 3;
+echo $pesan->variable;
+$pesan->variable =3.5;
+echo $pesan->variable;
+
+class Sapa {
+
+var $variable;
+
+} 
+
+
+
+echo '<br>';
+
+$variable = "Hello Mabes TNI ";
+echo $variable;
+$variable = 3;
+echo $variable;
+$variable = 3.5;
+echo $variable;
+
+
+?>
