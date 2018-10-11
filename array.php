cd961dc32e37d9146b845482a371f8c0704ac8f5
Putri1987 <taniamischa87@gmail.com>
2018-10-12 00:26:56 +0700

    arrayku

---
+++ array.php
@@ -0,0 +1,15 @@
+
+<?php
+$variable1 = "Hello";
+$variable2 = "Mabes TNI";
+echo $variable1; echo $variable2;
+
+
+echo '<br>';
+
+$variable = array("Hello", "Mabes TNI");
+echo $variable [0];
+echo $variable[1];
+
+?>
+
